0818cc09feedff2f31b1b4901cec9a10b800aa44
muhammadamin-web <mirboqijonov@gmail.com>
2024-08-18 21:05:52 +0500

    push project

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,10 +1,10 @@
-import logo from '../../assets/images/logo.png'
+import logo from '../../assets/images/logo2.svg'
 
 const Loading = () => {
   return (
     <div className="loading">
         <div className="loading_card">
-        <img src={logo} alt="" />
+        <img src={logo} alt="" width={'100%'} />
         <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
 
         </div>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -11,6 +11,7 @@ import { FaYoutube } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-scroll";
 import { IoMdClose } from "react-icons/io";
+import txt from "../../assets/images/txt.png";
 
 const Nav = () => {
   const [active, setActive] = useState(false);
@@ -19,136 +20,155 @@ const Nav = () => {
     <>
       <nav className="nav">
         <div className="container">
-          <div className="nav_box">
-            <div className="nav_card_left">
-              <a href="#!">
-                <img className="nav_logo" src={logo} alt="" />
-              </a>
-              <div className={`nav_list_menu ${active && "active"}`}>
-                <div className="container2">
-                  <div className="nav_list_card">
-                    <BiMenuAltLeft className="nav_menu_logo" />
-                    <a href="#!">
-                      <img className="nav_logo" src={logo} alt="" />
-                    </a>
-                    <IoMdClose
-                      className="nav_close"
-                      onClick={() => setActive(false)}
-                    />
-                  </div>
-                  <ul className="nav_list">
-                  <li>
-                      <Link
-                        to="about"
-                        smooth={true}
-                        duration={500}
-                        offset={-50}
-                        href="#!"
-                        className="nav_link"
+          <div className="nav_box_container">
+            <div className="nav_box">
+              <div className="nav_card_left">
+                <a href="#!">
+                  <img className="nav_logo" src={logo} alt="" />
+                </a>
+                <div className={`nav_list_menu ${active && "active"}`}>
+                  <div className="container2">
+                    <div className="nav_list_card">
+                      <BiMenuAltLeft className="nav_menu_logo" />
+                      <a href="#!">
+                        <img className="nav_logo" src={logo} alt="" />
+                      </a>
+                      <IoMdClose
+                        className="nav_close"
                         onClick={() => setActive(false)}
-                      >
-                        Loyiha haqida
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="average"
-                        smooth={true}
-                        duration={500}
-                        href="#!"
-                        className="nav_link"
-                        onClick={() => setActive(false)}
-                      >
-                        Bloklar joylashuvi
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="houses"
-                        smooth={true}
-                        duration={500}
-                        offset={-50}
-                        onClick={() => setActive(false)}
-                        href="#!" className="nav_link"
-                      >
-                        Xonadonlarni rejalashtirish
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="company"
-                         smooth={true}
-                        duration={500}
-                        onClick={() => setActive(false)}
-                        href="#!"
-                        className="nav_link"
-                      >
-                        Kompaniya haqida
-                      </Link>
-                    </li>
+                      />
+                    </div>
+                    <ul className="nav_list">
+                      <li>
+                        <Link
+                          to="about"
+                          smooth={true}
+                          duration={500}
+                          offset={-50}
+                          href="#!"
+                          className="nav_link"
+                          onClick={() => setActive(false)}
+                        >
+                          Loyiha haqida
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="average"
+                          smooth={true}
+                          duration={500}
+                          href="#!"
+                          className="nav_link"
+                          onClick={() => setActive(false)}
+                        >
+                          Bloklar joylashuvi
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="houses"
+                          smooth={true}
+                          duration={500}
+                          offset={-50}
+                          onClick={() => setActive(false)}
+                          href="#!"
+                          className="nav_link"
+                        >
+                          Xonadonlarni rejalashtirish
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="company"
+                          smooth={true}
+                          duration={500}
+                          onClick={() => setActive(false)}
+                          href="#!"
+                          className="nav_link"
+                        >
+                          Kompaniya haqida
+                        </Link>
+                      </li>
 
-                    <li>
-                      <Link
-                        to="footer"
-                        smooth={true}
-                        duration={500}
-                        offset={-50}
-                        href="#!"
-                        className="nav_link"
-                        onClick={() => setActive(false)}
+                      <li>
+                        <Link
+                          to="footer"
+                          smooth={true}
+                          duration={500}
+                          offset={-50}
+                          href="#!"
+                          className="nav_link"
+                          onClick={() => setActive(false)}
+                        >
+                          Bog‘lanish{" "}
+                        </Link>
+                      </li>
+                    </ul>
+                    <div className="nav_icons">
+                      <a
+                        href="https://www.facebook.com/txtgroup.uzb"
+                        className="nav_icons_card"
                       >
-                        Bog‘lanish{" "}
-                      </Link>
-                    </li>
-                  </ul>
-                  <div className="nav_icons">
-                    <a href="https://www.facebook.com/txtgroup.uzb" className="nav_icons_card">
-                      <FaFacebookF className="nav_icons_icon" />
-                    </a>
-                    <a href="https://www.instagram.com/txtgroup.uzb" className="nav_icons_card">
-                      <FaInstagram className="nav_icons_icon" />
-                    </a>
-                    <a href="https://t.me/txtgroupuzb" className="nav_icons_card">
-                      <FaTelegramPlane className="nav_icons_icon" />
-                    </a>
-                    <a href="!#" className="nav_icons_card">
-                      <FaYoutube className="nav_icons_icon" />
-                    </a>
-                  </div>
-                  <div className="nav_card_right menu">
-                    <div className="nav_contact">
-                      <PiPhoneCallLight className="nav_phone_icon" />
-                      <a href="tel: 1361" className="nav_contact_text">
-                        1361
+                        <FaFacebookF className="nav_icons_icon" />
+                      </a>
+                      <a
+                        href="https://www.instagram.com/txtgroup.uzb"
+                        className="nav_icons_card"
+                      >
+                        <FaInstagram className="nav_icons_icon" />
+                      </a>
+                      <a
+                        href="https://t.me/txtgroupuzb"
+                        className="nav_icons_card"
+                      >
+                        <FaTelegramPlane className="nav_icons_icon" />
+                      </a>
+                      <a href="!#" className="nav_icons_card">
+                        <FaYoutube className="nav_icons_icon" />
                       </a>
                     </div>
-                    <div className="nav_download">
-                      <RxDownload className="nav_phone_icon" />
-                      <div className="nav_download_card">
-                        <p className="nav_download_text1">Taqdimot</p>
-                        <p className="nav_download_text2">
-                          5.3 mb yuklab olish
-                        </p>
+                    <div className="nav_card_right menu">
+                      <div className="nav_contact">
+                        <PiPhoneCallLight className="nav_phone_icon" />
+                        <a href="tel: 1361" className="nav_contact_text">
+                          1361
+                        </a>
+                      </div>
+                      <div className="nav_download">
+                        <RxDownload className="nav_phone_icon" />
+                        <div className="nav_download_card">
+                          <p className="nav_download_text1">Taqdimot</p>
+                          <p className="nav_download_text2">
+                            5.3 mb yuklab olish
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="nav_card_right">
-              <div className="nav_contact">
-                <PiPhoneCallLight className="nav_phone_icon" />
-                <a href="tel: 1361" className="nav_contact_text">
-                1361
-                </a>
-                <IoMenu className="nav_menu" onClick={() => setActive(true)} />
-              </div>
-              <div className="nav_download download">
-                <RxDownload className="nav_phone_icon" />
-                <div className="nav_download_card">
-                  <p className="nav_download_text1">Taqdimot</p>
-                  <p className="nav_download_text2">5.3 mb yuklab olish </p>
+              <div className="nav_card_right">
+                <div className="nav_contact">
+                  <PiPhoneCallLight className="nav_phone_icon" />
+                  <a href="tel: 1361" className="nav_contact_text">
+                    1361
+                  </a>
+                  <IoMenu
+                    className="nav_menu"
+                    onClick={() => setActive(true)}
+                  />
+                </div>
+                <div className="nav_download download">
+                  <RxDownload className="nav_phone_icon" />
+                  <div className="nav_download_card">
+                    <p className="nav_download_text1">Taqdimot</p>
+                    <p className="nav_download_text2">5.3 mb yuklab olish </p>
+                  </div>
                 </div>
               </div>
+            </div>
+            <div className="nav_txt">
+              <img src={txt} alt="" className="nav_txt_img" />
             </div>
           </div>
         </div>
